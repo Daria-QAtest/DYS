510f5abb01af191f5456b78d6324847665ee2866
Daria-QAtest <darya.meshkova26@mail.ru>
2025-04-02 19:53:02 +0300

    Diplom”
    git commit -m Diplom”

---
+++ configuration.py
@@ -0,0 +1,3 @@
+URL_SERVICE = "https://764aa855-d95d-48ca-aa68-3325ef3bb60f.serverhub.praktikum-services.ru/"
+CREATE_ORDER = "/api/v1/orders"
+ORDER_INFORMATION = "/api/v1/orders/track"
---
+++ data.py
@@ -0,0 +1,11 @@
+order_body = {
+        "firstName": "Мария",
+        "lastName": "Иванова",
+        "address": "Малаховка, дом 5",
+        "metroStation": 80,
+        "phone": "+79168593684",
+        "rentTime": 3,
+        "deliveryDate": "2025-04-04",
+        "comment": "Очень жду",
+        "color": ["BLACK"]
+    }
